28d92bf3ec9f1121144c5414648b20d0cc664bb8
Boone Epstein <booneepstein@gmail.com>
2021-01-05 18:57:56 -0600

    Fix existing user tests and methods

--- test/user-test.js
+++ test/user-test.js
@@ -3,59 +3,114 @@ import { expect } from 'chai';
 import User from '../src/user';
 import users from '../src/data/users-data';
 
-describe('User', function() {
+describe('User', () => {
   let user;
   let userInfo;
-  let recipe;
+  let recipe1;
+  let recipe2;
+  let recipe3;
 
-  beforeEach(function() {
+  beforeEach(() => {
     userInfo = users[0];
-    user = new User(userInfo)
+    user = new User(userInfo);
 
-    recipe = {name: 'Chicken Parm', type: ['italian', 'dinner']};
+    recipe1 = {name: 'Chicken Parm', type: ['italian', 'dinner']};
+    recipe2 = {name: 'Somen', type: ['japanese', 'lunch']};
+    recipe3 = {name: 'Yakisoba', type: ['japanese', 'lunch']};
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(User).to.be.a('function');
   });
 
-  it('should initialize with an id', function() {
+  it('should initialize with an id', () => {
     expect(user.id).to.eq(1);
   });
 
-  it('should initialize with a name', function() {
+  it('should initialize with a name', () => {
     expect(user.name).to.eq('Saige O\'Kon');
   });
 
-  it('should initialize with a pantry', function() {
-    expect(user.pantry[0].ingredient).to.eq(11477);
+  it('should initialize with a pantry', () => {
+    expect(user.pantry).to.eql(userInfo.pantry);
   });
 
-  it('should initialize with an empty favoriteRecipes array', function() {
-    expect(user.favoriteRecipes).to.deep.equal([]);
+  it('should initialize with an empty favoriteRecipes array', () => {
+    expect(user.favoriteRecipes).to.eql([]);
   });
 
-  it('should initialize with an empty recipesToCook array', function() {
-    expect(user.recipesToCook).to.deep.equal([]);
+  it('should initialize with an empty recipesToCook array', () => {
+    expect(user.recipesToCook).to.eql([]);
   });
 
-  it('should be able to save a recipe to favoriteRecipes', function() {
-    user.saveRecipe(recipe);
-    expect(user.favoriteRecipes[0].name).to.equal('Chicken Parm');
+  it('should be able to save a recipe to favoriteRecipes', () => {
+    user.addRecipe('favoriteRecipes', recipe1);
+    expect(user.favoriteRecipes).to.eql([recipe1]);
+
+    user.addRecipe('favoriteRecipes', recipe2);
+    expect(user.favoriteRecipes).to.eql([recipe1, recipe2]);
   });
 
-  it('should be able to decide to cook a recipe', function() {
-    user.decideToCook(recipe);
-    expect(user.recipesToCook[0].name).to.equal('Chicken Parm');
+  it('should not save the same recipe to favoriteRecipes multiple times', () => {
+    user.addRecipe('favoriteRecipes', recipe1);
+    user.addRecipe('favoriteRecipes', recipe1);
+    expect(user.favoriteRecipes).to.eql([recipe1]);
   });
 
-  it('should be able to filter recipes by type', function() {
-    user.saveRecipe(recipe);
-    expect(user.filterRecipes('italian')).to.deep.equal([recipe]);
+  it('should be able to remove recipes from favoriteRecipes', () => {
+    user.addRecipe('favoriteRecipes', recipe1);
+    user.addRecipe('favoriteRecipes', recipe2);
+    user.removeRecipe('favoriteRecipes', recipe1);
+    expect(user.favoriteRecipes).to.eql([recipe2]);
+
+    user.removeRecipe('favoriteRecipes', recipe2);
+    expect(user.favoriteRecipes).to.eql([])
   });
 
-  it('should be able to search recipes by name', function() {
-    user.saveRecipe(recipe);
-    expect(user.searchForRecipe('Chicken Parm')).to.deep.equal([recipe]);
+  it('should be able to remove recipes from recipesToCook', () => {
+    user.addRecipe('recipesToCook', recipe1);
+    user.addRecipe('recipesToCook', recipe2);
+    user.removeRecipe('recipesToCook', recipe1);
+    expect(user.recipesToCook).to.eql([recipe2]);
+
+    user.removeRecipe('recipesToCook', recipe2);
+    expect(user.recipesToCook).to.eql([])
   });
+
+  it('should be able to decide to cook a recipe', () => {
+    user.addRecipe('recipesToCook', recipe1);
+    expect(user.recipesToCook).to.eql([recipe1]);
+
+    user.addRecipe('recipesToCook', recipe2);
+    expect(user.recipesToCook).to.eql([recipe1, recipe2])
+  });
+
+  it('should be able to filter favoriteRecipes by type', () => {
+    user.addRecipe('favoriteRecipes', recipe1);
+    user.addRecipe('favoriteRecipes', recipe2);
+
+    expect(user.filterRecipes('favoriteRecipes', 'type', 'italian')).to.deep.equal([recipe1]);
+
+    user.addRecipe('favoriteRecipes', recipe3)
+    expect(user.filterRecipes('favoriteRecipes', 'type', 'japanese')).to.deep.equal([recipe2, recipe3]);
+  });
+
+  it('should be able to filter recipesToCook by type', () => {
+    user.addRecipe('recipesToCook', recipe1);
+    user.addRecipe('recipesToCook', recipe2);
+
+    expect(user.filterRecipes('recipesToCook', 'type', 'italian')).to.deep.equal([recipe1]);
+
+    user.addRecipe('recipesToCook', recipe3);
+    expect(user.filterRecipes('recipesToCook', 'type', 'japanese')).to.deep.equal([recipe2, recipe3]);
+  });
+
+  it('should be able to filter favoriteRecipes by name', () => {
+    user.addRecipe('favoriteRecipes', recipe1);
+    user.addRecipe('favoriteRecipes', recipe2);
+    user.addRecipe('favoriteRecipes', recipe3);
+
+    expect(user.filterRecipes('favoriteRecipes', 'name', 'Chicken Parm')).to.deep.equal([recipe1]);
+  });
+
 });
